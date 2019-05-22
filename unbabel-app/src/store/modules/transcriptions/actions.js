aeb8c1be698d9752fd6aca44c8f764ff8552e81d
Lawrence <lawbraun.almeida@gmail.com>
2019-05-23 00:53:29 +0100

    finish store mechanics; other minor style & design enhancements

--- unbabel-app/src/store/modules/transcriptions/actions.js
+++ unbabel-app/src/store/modules/transcriptions/actions.js
@@ -4,5 +4,8 @@ import API from '@/api/api'
 export default {
   GET_TRANSCRIPTS ({ }) {
     return API.GET.transcripts()
+  },
+  PUSH_TRANSCRIPTS ({ }) {
+    return API.POST.transcripts()
   }
 }
--- unbabel-app/src/store/modules/transcriptions/mutations.js
+++ unbabel-app/src/store/modules/transcriptions/mutations.js
@@ -8,20 +8,37 @@ const transcriptIndex = (state, id) => {
 
 export default {
   saveTranscripts (state, value) {
-    Vue.set(state, 'transcripts', [...value])
-  },
-  toggleSelection (state, data) {
-    let indexPos = transcriptIndex(state, data.id)
-
-    if (indexPos >= 0) {
-      state.transcripts[indexPos].selected = data.selected
+    if (state.transcripts.length === 0) {
+      Vue.set(state, 'transcripts', [
+        ...value
+      ])
+    } else {
+      value.map(item => {
+        //  Weak, but avoids pushing an item with the same ID already
+        if (transcriptIndex(state, item.id) < 0) {
+          state.transcripts.push(item)
+        }
+      })
     }
+  },
+  addItem (state, data) {
+    //  Just a random ID > 3
+    data.id = Math.floor(Math.random() * 1000) + 4
+
+    data.selected = false
+
+    state.transcripts.push(data)
   },
   updateField (state, data) {
     let indexPos = transcriptIndex(state, data.id)
-
     if (indexPos >= 0) {
       state.transcripts[indexPos][data.field] = data.value
+    }
+  },
+  removeItem (state, data) {
+    let indexPos = transcriptIndex(state, data.id)
+    if (indexPos >= 0) {
+      state.transcripts.splice(indexPos, 1)
     }
   }
 }
