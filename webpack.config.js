745a39d21d165534da62a0df68139e143a580294
João Bicho <joaorbicho@gmail.com>
2019-07-17 17:41:19 +0100

    Initial Configuration
    Created the basic project structure using react, redux, saga, sass and npm

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+
+module.exports = env => {
+  let sourceMap = undefined;
+
+  if (env === "development") {
+    sourceMap = true;
+  }
+
+  if (env === "production") {
+    sourceMap = false;
+  }
+
+  const config = {
+    stats: "verbose",
+    entry: "./src/index.tsx",
+    output: {
+      filename: "./assets/[name].[hash].js",
+      sourceMapFilename: "./assets/[name].[hash].js.map",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: process.env.PUBLIC_PATH || "/"
+    },
+    devtool: "source-map",
+    devServer: {
+      port: 8080,
+      host: "front-end-challenge",
+      historyApiFallback: true,
+      inline: true,
+      contentBase: path.resolve(__dirname, "dist"),
+      publicPath: process.env.PUBLIC_PATH || "/",
+      disableHostCheck: true,
+      stats: "errors-only"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json", ".scss"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader"
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "style-loader",
+              options: {
+                sourceMap: sourceMap
+              }
+            },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: sourceMap
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: sourceMap,
+                includePaths: [path.resolve("./styles")]
+              }
+            }
+          ]
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all"
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Front-End Challenge",
+        filename: "index.html",
+        template: path.resolve(__dirname, "./public/index.html")
+      }),
+      new ZipPlugin({
+        path: "zip",
+        filename: "front-end-challenge.zip",
+        exclude: ["config.json"]
+      })
+    ]
+  };
+
+  return [config];
+};
