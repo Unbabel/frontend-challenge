4873366520f54d371414fcae78521226d7e676f1
Ronaiza Cardoso <ronaiza.cardoso@co.pdmfc.com>
2019-07-20 14:04:10 +0100

    style: header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,12 +1,16 @@
 <template>
     <nav>
         <h1 class="Logo">Transcriptions</h1>
-        <button>
-            <img src="../../assets/add-row.svg" alt="Get data">
-        </button>
-        <button>
-            <img src="../../assets/fetch-document.svg" alt="Upload data">
-        </button>
+        <div class="btn-container" aria-labelledby="Button container">
+            <button>
+                <img src="../../assets/upload.svg" aria-hiden="true">
+                <span class="sr-only">Upload data</span>
+            </button>
+            <button>
+                <img src="../../assets/fetch-document.svg" aria-hiden="true">
+                <span class="sr-only">Get data</span>
+            </button>
+        </div>
     </nav>
 </template>
 
@@ -17,3 +21,31 @@ export default {
 }
 
 </script>
+
+<style lang="scss" scoped>
+    nav {
+        background: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        padding: 1rem;
+        transition: box-shadow 0.5s;
+
+        &:hover {
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+        }
+
+        h1 {
+            font-family: 'Montserrat', sans-serif;
+            font-weight: var(--medium);
+            font-size: 18px;
+            color: var(--title-color);
+        }
+    }
+
+    button {
+        border: none;
+        margin-right: 1rem;
+        padding: 5px 1rem;
+    }
+</style>
