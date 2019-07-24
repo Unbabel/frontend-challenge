438c8407c4358edee78e1086ed3763d2b754adb2
David Maia <david.maia@mash.com>
2019-07-24 17:38:16 +0100

    add initial header. add basic web font handling

---
+++ components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./Header";
+
+const App = () => {
+  return <Header />;
+};
+
+export default App;
--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,35 @@
 import React from "react";
-import Link from "next/link";
+import FetchDocument from "../static/svg/fetch-document.svg";
+import Upload from "../static/svg/upload.svg";
 
 const Header = () => (
-  <div>
-    <Link href="/">
-      <a>Transcriptions</a>
-    </Link>
+  <div className="container">
+    <div>Transcriptions</div>
+    <div className="icons">
+      <FetchDocument />
+      <Upload />
+    </div>
+    <style jsx>
+      {`
+        .container {
+          background: white;
+          box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.24),
+            0 1px 3px 0 rgba(0, 0, 0, 0.12);
+          width: 100%;
+          height: 64px;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          font-family: Montserrat;
+          font-size: 18px;
+          font-weight: 600;
+        }
+
+        .icons svg {
+          margin-right: 24px;
+        }
+      `}
+    </style>
   </div>
 );
 
