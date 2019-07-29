8b4c6449f28fe11621970c2de0da8c4616c04661
David Maia <david.maia@mash.com>
2019-07-29 18:13:16 +0100

    Add support for remove. Small refactor

--- components/Transcriptions.js
+++ components/Transcriptions.js
@@ -3,97 +3,134 @@ import { useStore } from './store';
 import Person from '../static/svg/person.svg';
 import AddRow from '../static/svg/add-row.svg';
 import Delete from '../static/svg/delete.svg';
-import { updateExpression } from '@babel/types';
 
 const Transcriptions = () => {
   const [store, dispatch] = useStore();
   const transcriptions = Object.values(store.transcriptions);
   const [editable, setEditable] = useState({});
+  const [deletable, setDeletable] = useState({});
 
-  const editTranscription = useCallback(
-    ({ id, type }) => () =>
-      // there can be only one currently editable transcription
-      setEditable({ [id]: true, [type]: true }),
-    [setEditable]
-  );
-  const updateTranscription = useCallback(
-    ({ id, type }) => ({ target: { textContent } }) =>
-      dispatch({
-        type: 'update',
-        payload: { id, [type]: textContent },
-      }),
-    [dispatch]
-  );
-  const addTranscription = useCallback(
+  const createTranscription = useCallback(
     () =>
       dispatch({
         type: 'create',
       }),
     [dispatch]
   );
+  const updateTranscription = useCallback(
+    (id, type) => ({ target: { textContent } }) =>
+      dispatch({
+        type: 'update',
+        payload: { id, [type]: textContent },
+      }),
+    [dispatch]
+  );
+  const deleteTranscription = useCallback(
+    id => () =>
+      dispatch({
+        type: 'delete',
+        payload: { id },
+      }),
+    [dispatch]
+  );
+
+  const setEditTranscription = useCallback(
+    (id, type) => () => setEditable({ [id]: type }),
+    [setEditable]
+  );
+  const setDeleteTranscription = useCallback(
+    (id, deletable) => () => setDeletable({ [id]: deletable }),
+    [setDeletable]
+  );
 
   return (
     <>
       {transcriptions.length > 0 && (
         <>
-          <div className='container'>
-            <div className='grid'>
-              {transcriptions.map(({ id, text, voice }, idx, arr) => {
-                return (
-                  <Fragment key={id}>
-                    <div className='checkbox'>
-                      <input type='checkbox' />
+          <div className='grid'>
+            {transcriptions.map(({ id, text, voice }, idx, arr) => {
+              return (
+                <div
+                  key={id}
+                  onMouseEnter={setDeleteTranscription(id, true)}
+                  onMouseLeave={setDeleteTranscription(id, false)}
+                  className='grid-item'
+                >
+                  <div className='checkbox'>
+                    <input type='checkbox' />
+                  </div>
+                  <div className='icon'>
+                    <Person />
+                  </div>
+                  <div
+                    className='voice'
+                    onClick={setEditTranscription(id, 'voice')}
+                    onBlur={updateTranscription(id, 'voice')}
+                    contentEditable={editable[id] === 'voice'}
+                    suppressContentEditableWarning
+                  >
+                    {voice}
+                  </div>
+                  <div
+                    className='text'
+                    onClick={setEditTranscription(id, 'text')}
+                    onBlur={updateTranscription(id, 'text')}
+                    contentEditable={editable[id] === 'text'}
+                    suppressContentEditableWarning
+                  >
+                    {text}
+                  </div>
+                  {deletable[id] && (
+                    <div className='delete' onClick={deleteTranscription(id)}>
+                      <Delete />
                     </div>
-                    <div className='icon'>
-                      <Person />
-                    </div>
-                    <div
-                      className='voice'
-                      onClick={editTranscription({ id, type: 'voice' })}
-                      onBlur={updateTranscription({ id, type: 'voice' })}
-                      contentEditable={
-                        Object.prototype.hasOwnProperty.call(editable, id) &&
-                        editable.voice
-                      }
-                      suppressContentEditableWarning
-                    >
-                      {voice}
-                    </div>
-                    <div
-                      className='text'
-                      onClick={editTranscription({ id, type: 'text' })}
-                      onBlur={updateTranscription({ id, type: 'text' })}
-                      contentEditable={
-                        Object.prototype.hasOwnProperty.call(editable, id) &&
-                        editable.text
-                      }
-                      suppressContentEditableWarning
-                    >
-                      {text}
-                    </div>
-                    {arr.length - 1 !== idx && <hr className='separator' />}
-                  </Fragment>
-                );
-              })}
-            </div>
+                  )}
+                </div>
+              );
+            })}
           </div>
-          <div className='add-row' onClick={addTranscription}>
+          <div className='add-row' onClick={createTranscription}>
             <AddRow />
           </div>
         </>
       )}
       <style jsx>
         {`
-          .container {
+          .grid {
+            border: 1px #eaedef solid;
             margin: 10vh 40vh 0 40vh;
-            padding: 24px;
-            border: 1px #eaedef inset;
           }
 
-          .grid {
+          .grid-item {
             display: grid;
-            grid-template-columns: [checkbox] 0fr [icon] 0fr [content] auto;
+            grid-template-columns: [checkbox] 0fr [icon] 0fr [content] auto [delete] min-content;
             grid-column-gap: 16px;
+            padding: 32px 24px;
+            border-bottom: 1px #eaedef solid;
+          }
+
+          .grid-item:hover {
+            box-shadow: 0 0 2px #859eff;
+          }
+
+          .hover {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: -24px;
+            left: -24px;
+          }
+
+          .delete {
+            grid-column: delete;
+            grid-row: 1;
+            align-self: center;
+            justify-self: center;
+            cursor: pointer;
+          }
+
+          .delete :global(svg) {
+            display: block;
           }
 
           .checkbox,
@@ -114,10 +151,6 @@ const Transcriptions = () => {
 
           .voice:global(:focus),
           .text:gloval(:focus) {
-            box-shadow: 0 0 5px rgba(81, 203, 238, 1);
-            padding: 3px 0px 3px 3px;
-            margin: 5px 1px 3px 0px;
-            border: 1px solid rgba(81, 203, 238, 1);
           }
 
           .error {
@@ -126,12 +159,6 @@ const Transcriptions = () => {
 
           .icon :global(svg) {
             display: block;
-          }
-
-          .separator {
-            grid-column: 1 / -1;
-            margin: 24px -24px;
-            border: 1px #eaedef solid;
           }
 
           .add-row {
