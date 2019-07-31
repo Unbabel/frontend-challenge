2c98ec92215f5f8b072cb5b03d7dbefbdee3d211
Miguel Carvalho <miguel@brpx.com>
2019-07-31 17:38:33 +0100

    Add component List Item

---
+++ app/components/ListItem/Item.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  align-items: flex-start;
+`;
+
+export default Item;
