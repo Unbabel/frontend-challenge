de3cf0447f19e12af1c8f21a5a2db14f183aaec0
Miguel Carvalho <miguel@brpx.com>
2019-07-31 21:23:48 +0100

    Add add voice button placeholder

---
+++ app/components/TranscriptionList/Div.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import Button from 'components/Button';
+
+const Div = styled.div`
+  position: relative;
+
+  > div:nth-child(2) {
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+export default Div;
--- app/components/TranscriptionList/index.js
+++ app/components/TranscriptionList/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Button from 'components/Button';
+import Icon from 'components/Icon';
 import List from 'components/List';
 import ListItem from 'components/ListItem';
 import LoadingIndicator from 'components/LoadingIndicator';
 import TranscriptionListItem from 'containers/TranscriptionListItem';
+
+import Div from './Div';
 
 function TranscriptionList({ loading, error, transcriptions }) {
   if (loading) {
@@ -12,14 +16,19 @@ function TranscriptionList({ loading, error, transcriptions }) {
   }
 
   if (error !== false) {
-    const ErrorComponent = () => (
-      <ListItem item="Something went wrong, please try again!" />
-    );
+    const ErrorComponent = () => <ListItem item="Something went wrong, please try again!" />;
     return <List component={ErrorComponent} />;
   }
 
   if (transcriptions !== false) {
-    return <List items={transcriptions} component={TranscriptionListItem} />;
+    return (
+      <Div>
+        <List items={transcriptions} component={TranscriptionListItem} />
+        <Button>
+          <Icon name="add" />
+        </Button>
+      </Div>
+    );
   }
 
   return null;
