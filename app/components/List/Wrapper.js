85fe7fbde47a801976e23d9b62576b332907e132
Miguel Carvalho <miguel@brpx.com>
2019-07-31 17:40:16 +0100

    Add List component

---
+++ app/components/List/Wrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  background-color: white;
+  border: 1px solid #ccc;
+  overflow: hidden;
+`;
+
+export default Wrapper;
