d9627a2d75fadb2eab83f61f09c3f53aa441c1fc
Miguel Carvalho <miguel@brpx.com>
2019-07-31 23:26:01 +0100

    Implemnts svg with use on the Icon component

--- app/components/Icon/index.js
+++ app/components/Icon/index.js
@@ -1,16 +1,32 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
+import PropTypes from 'prop-types';
 
-function requireAll(r) {
-  r.keys().forEach(r);
-}
+import { COLORS } from '../../theme';
 
-requireAll(require.context('../../../challenge-info', true, /\.svg$/));
-
+import Div from './Div';
 import Svg from './Svg';
 
-function Icon({ name }) {
-  return <Svg theme={useContext(ThemeContext)}>{name}</Svg>;
+function Icon(props) {
+  return (
+    <Div {...props} theme={useContext(ThemeContext)}>
+      <Svg width={`${props.size}px`} height={`${props.size}px`}>
+        <use xlinkHref={`#${props.name}`} />
+      </Svg>
+    </Div>
+  );
 }
+
+Icon.propTypes = {
+  name: PropTypes.oneOf(['add-row', 'avatar', 'fetch-document', 'person', 'upload']).isRequired,
+  color: PropTypes.oneOf([COLORS.BLUE, COLORS.GREY_DARK, COLORS.GREY_LIGHT, COLORS.GREY_LIGHTER]),
+  size: PropTypes.number,
+  viewBox: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  color: COLORS.GREY_DARK,
+  size: 32,
+};
 
 export default Icon;
