3f2d47ffb3f0fb978d64909b8a10bc733149b137
Miguel Carvalho <miguel@brpx.com>
2019-08-01 16:18:40 +0100

    Add an animation to Logo Component on hover

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -4,7 +4,24 @@ import { ThemeContext } from 'styled-components';
 import H1 from './H1';
 
 function Logo() {
-  return <H1 theme={useContext(ThemeContext)}>Transcriptions</H1>;
+  return (
+    <H1 theme={useContext(ThemeContext)}>
+      <span>T</span>
+      <span>r</span>
+      <span>a</span>
+      <span>n</span>
+      <span>s</span>
+      <span>c</span>
+      <span>r</span>
+      <span>i</span>
+      <span>p</span>
+      <span>t</span>
+      <span>i</span>
+      <span>o</span>
+      <span>n</span>
+      <span>s</span>
+    </H1>
+  );
 }
 
 export default Logo;
