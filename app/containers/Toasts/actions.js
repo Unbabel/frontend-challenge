d63e721226082aa280b0fbfc65f0e521c37912b9
Miguel Carvalho <miguel@brpx.com>
2019-08-01 13:58:45 +0100

    Update Toast container structure

--- app/containers/Toasts/actions.js
+++ app/containers/Toasts/actions.js
@@ -1,6 +1,6 @@
 /*
  *
- * Toasts actions
+ * Alerts actions
  *
  */
 
--- app/containers/Toasts/index.js
+++ app/containers/Toasts/index.js
@@ -5,12 +5,34 @@
  */
 
 import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
+import Toast from 'components/Toast';
+
 import makeSelectToasts from './selectors';
 import { hideToast } from './actions';
+import messages from './messages';
+
+import Div from './Div';
+
+export function Toasts({ toasts, hideItem }) {
+  return (
+    <Div>
+      {toasts.map((toast, index) => (
+        <Toast hideItem={() => hideItem(toast)} toast={toast} key={`toast-${index}`} />
+      ))}
+    </Div>
+  );
+}
+
+Toasts.propTypes = {
+  toasts: PropTypes.array,
+  hideItem: PropTypes.func,
+};
 
 const mapStateToProps = createStructuredSelector({
   toasts: makeSelectToasts(),
@@ -18,7 +40,7 @@ const mapStateToProps = createStructuredSelector({
 
 function mapDispatchToProps(dispatch) {
   return {
-    hideToast: toast => dispatch(hideToast(toast)),
+    hideItem: toast => dispatch(hideToast(toast)),
   };
 }
 
