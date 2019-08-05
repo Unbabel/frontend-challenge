b91d5249e144e502e3041e353e07c5590322033f
Miguel Carvalho <miguel@brpx.com>
2019-08-05 11:40:44 +0100

    Add initial structure of the List component

---
+++ src/components/List/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li>transcription 1</li>
+    <li>transcription 2</li>
+    <li>transcription 3</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "List"
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Header />
+    <List />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import List from "@/components/List";
 
 export default {
   name: "home",
   components: {
-    Header
+    Header,
+    List
   }
 };
 </script>
