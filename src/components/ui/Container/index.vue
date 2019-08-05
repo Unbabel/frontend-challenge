f1a935fd7c67e7caaa4c8f5898c2db304fc78c85
Miguel Carvalho <miguel@brpx.com>
2019-08-05 23:59:44 +0100

    Simplify Container component

--- src/components/ui/Container/index.vue
+++ src/components/ui/Container/index.vue
@@ -4,25 +4,6 @@
   </div>
 </template>
 
-<script>
-import { mapMutations } from "vuex";
-import { MUTATIONS } from "@/store/modules/transcriptions/constants";
-export default {
-  name: "Container",
-
-  props: {
-    item: {
-      required: true,
-      type: Object
-    }
-  },
-
-  methods: mapMutations("transcriptions", {
-    deleteItem: MUTATIONS.DELETE
-  })
-};
-</script>
-
 <style scoped lang="scss">
 div {
   max-width: 738px;
