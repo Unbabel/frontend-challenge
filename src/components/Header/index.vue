d2ad805fa78d1151fb8e3bfe41a95406692e193f
Miguel Carvalho <miguel@brpx.com>
2019-08-05 11:37:55 +0100

    Using Header component in Home view

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -14,24 +14,10 @@ export default {
 };
 </script>
 
-<style scoped>
-nav {
-  background: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  padding: 1rem;
-  transition: box-shadow 0.5s;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-
-  &:hover {
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
-  }
-  h1 {
-    font-family: "Montserrat", sans-serif;
-    font-weight: var(--medium);
-    font-size: 18px;
-    color: var(--title-color);
+<style scoped lang="scss">
+header {
+  nav {
+    border: 1px solid red;
   }
 }
 </style>
