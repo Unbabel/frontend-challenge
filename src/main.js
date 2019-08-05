4c5a41847a58c9febf11bedcb56a2650b5d9f991
Miguel Carvalho <miguel@brpx.com>
2019-08-05 12:27:26 +0100

    Move all initialization files to a internals folder

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import router from "./internals/router";
+import store from "./internals/store";
+import "./internals/registerServiceWorker";
 
 Vue.config.productionTip = false;
 
