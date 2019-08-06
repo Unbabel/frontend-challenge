719939400b8e35613d02481f810a29e7f914b5d8
Miguel Carvalho <miguel@brpx.com>
2019-08-06 02:57:09 +0100

    Add fontfaceobserver

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import FontFaceObserver from "fontfaceobserver";
+
 import Vue from "@/internals/config/vue";
 import router from "@/internals/config/router";
 import store from "@/store";
@@ -6,6 +8,17 @@ import "@/internals/registerServiceWorker";
 import "@/assets/scss/main.scss";
 
 import App from "@/App.vue";
+
+// Observe loading of Open Sans and Montserrat
+const openSansObserver = new FontFaceObserver("Open Sans", {});
+const montserratObserver = new FontFaceObserver("Montserrat", {});
+
+// When Open Sans is loaded, add a font-family using Open Sans to the body
+Promise.all([openSansObserver.load(), montserratObserver.load()]).then(
+  function addBodyClass() {
+    document.body.classList.add("fontsLoaded");
+  }
+);
 
 Vue.config.productionTip = false;
 
