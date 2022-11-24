c85e1bb8706a7788ea7f68ee7b6d1fce23db92a7
Thanoskaloudis <8anos.kal@gmail.com>
2022-11-24 18:06:28 +0000

    setup store and create actions and mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
-import { IData } from '@/utils/Data.model'
-import { createStore } from 'vuex'
+import { IData } from '@/utils/Data.model';
+import { createStore } from 'vuex';
+import * as DataAPI from "../utils/dataAPI";
 
 interface State {
   transcriptions: IData[];
@@ -10,9 +11,41 @@ export default createStore<State>({
     transcriptions: []
   },
   mutations: {
+    async GET_TRANSCRIPTIONS (state) {
+      try {
+        const res = await DataAPI.getData();
+        state.transcriptions.push(res);
+      } catch(error) {
+        console.log('Fetch error: ', error);
+      }
+    },
+    ADD_TRANSCRIPTIONS (state, transcription: IData) {
+      state.transcriptions.push(transcription)
+    },
+    DELETE_TRANSCRIPTIONS (state, transcription: IData) {
+      state.transcriptions = state.transcriptions.filter(t => t !== transcription);
+    },
+    async POST_TRANSCRIPTIONS (state) {
+      try {
+        const res = await DataAPI.postData(state.transcriptions);
+        console.log(res);
+      } catch(error) {
+        console.log('Fetch error: ', error);
+      }
+    }
   },
   actions: {
+    getTranscriptions ({commit}) {
+      commit('GET_TRANSCRIPTIONS');
+    },
+    addTranscriptions ({commit}, transcription: IData) {
+      commit('ADD_TRANSCRIPTIONS', transcription);
+    },
+    deleteTranscriptions ({commit}, transcription: IData) {
+      commit('DELETE_TRANSCRIPTIONS', transcription);
+    },
+    postTranscriptions ({commit}) {
+      commit('POST_TRANSCRIPTIONS');
+    }
   },
-  modules: {
-  }
 })
