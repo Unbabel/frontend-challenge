b5f80f5e28230680a4b16f99f7fe9e0c44cede10
Gabriel Rosa <gabrieldsrosa@gmail.com>
2022-11-24 04:35:26 +0000

    feat: add unit test

---
+++ unbabel-transcriptions/src/__tests__/Main.test.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+import Main from '../components/Main.vue';
+
+describe('Main Component', () => {
+  it('should render Main without crash', () => {
+    const wrapper = mount(Main);
+
+    expect(wrapper.text()).toContain('Transcriptions');
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
