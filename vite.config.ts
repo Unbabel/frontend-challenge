e58144ce9109c2fefeb152bce70e181e56809a1f
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-12-15 23:58:43 +0300

    Formatting files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 9090,
   },
+
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [vue()],
+
+  plugins: [vue(), svgLoader()],
 });
