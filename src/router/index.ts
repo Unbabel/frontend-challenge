391f13ee7ba055f13f883fc8349bed36f314ab2b
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-12-17 22:18:18 +0300

    Rename view from home to transcriptions

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TranscriptionsView from '../views/TranscriptionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'transcriptions',
+      component: TranscriptionsView,
     },
   ],
 })
