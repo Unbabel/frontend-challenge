b329b60450ad1feda482feaf8bf1b30d0b1674c0
Matheus Possas <matheuspossas98@gmail.com>
2023-01-10 16:17:42 +0000

    build: project scaffold

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Header />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from './components/Header.vue'
+
+export default defineComponent({
+  components: { Header }
+})
+</script>
