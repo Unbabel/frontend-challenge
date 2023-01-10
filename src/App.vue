0a1ace41ff5c5edec06ca6d7f8da250913197239
Matheus Possas <matheuspossas98@gmail.com>
2023-01-10 16:21:50 +0000

    build: add path alias

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Header from './components/Header.vue'
+import Header from '@/components/Header.vue'
 
 export default defineComponent({
   components: { Header }
