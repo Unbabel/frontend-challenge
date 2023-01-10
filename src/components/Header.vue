afd3e3bb12b58df14d7f6b9740a26d3af675b8b8
Matheus Possas <matheuspossas98@gmail.com>
2023-01-10 21:33:46 +0000

    feat: add header buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,48 +1,80 @@
 <template>
-    <header>
-        <div class="header-content">
-            <span>{{ $t('title') }}</span>
-            <button>Button</button>
-        </div>
-    </header>
+	<header class="header">
+		<div class="header__content">
+			<span class="header__title">{{ $t('title') }}</span>
+			<section class="header__buttons">
+				<button v-for="button in buttons">
+					<svg width="24" height="24">
+						<use :href="`src/assets/images/sprite.svg#${button.id}`" />
+					</svg>
+				</button>
+			</section>
+		</div>
+	</header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+	data: () => ({
+		buttons: [
+			{
+				id: 'upload'
+			},
+			{
+				id: 'fetch-document'
+			}
+		]
+	})
+})
 </script>
 
 <style lang="scss" scoped>
-header {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0;
+.header {
+	position: -webkit-sticky;
+	position: sticky;
+	top: 0;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
-    height: 69px;
-    background-color: white;
-    box-shadow: 0 3px 5px rgba(57, 63, 72, 0.3);
+	height: 69px;
+	background-color: white;
+	box-shadow: 0 3px 5px rgba(57, 63, 72, 0.3);
 
-    .header-content {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-        padding: 0 24px;
+	&__content {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		padding: 0 24px;
 
-        @media(min-width: 768px) {
-            max-width: 738px;
-        }
-    }
+		@media(min-width: 768px) {
+			max-width: 738px;
+		}
+	}
+
+	&__title {
+		font-family: 'Montserrat', sans-serif;
+		font-weight: 500;
+		font-size: 18px;
+		color: #414C5E;
+	}
+
+	&__buttons {
+		display: flex;
+	}
 }
 
-span {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 500;
-    font-size: 18px;
-    color: #414C5E;
+button {
+	display: flex;
+	align-items: center;
+	background: none;
+	border: none;
+	padding: 0;
+	cursor: pointer;
+	margin-left: 24px;
 }
 </style>
