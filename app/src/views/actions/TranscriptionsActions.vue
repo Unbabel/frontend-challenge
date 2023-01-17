c7e12e3166cce15c48441c3246d43871284db9c9
Pedro Santana Neves <pneves@bladeinsight.com>
2023-01-17 14:22:32 +0000

    add fetch action to fetch transcription icon

--- app/src/views/actions/TranscriptionsActions.vue
+++ app/src/views/actions/TranscriptionsActions.vue
@@ -1,10 +1,17 @@
 <script setup>
 import AppIcon from "@/components/base/AppIcon.vue";
+import { useTranscriptionsStore } from "../../store/useTranscriptionsStore";
+
+const { getTranscriptions } = useTranscriptionsStore();
 </script>
 
 <template>
   <div class="view-actions-container">
     <AppIcon icon-name="upload"></AppIcon>
-    <AppIcon icon-name="fetch-document"></AppIcon>
+    <AppIcon
+      icon-name="fetch-document"
+      button
+      @click="getTranscriptions"
+    ></AppIcon>
   </div>
 </template>
