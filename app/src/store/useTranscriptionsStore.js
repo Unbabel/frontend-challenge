7e6c03c4084a31233e608d297ef142d15ab7e08d
Pedro Santana Neves <pneves@bladeinsight.com>
2023-01-17 14:22:01 +0000

    update transcriptions store

--- app/src/store/useTranscriptionsStore.js
+++ app/src/store/useTranscriptionsStore.js
@@ -5,41 +5,41 @@
 import { computed, reactive } from "vue";
 import TranscriptionReadModel from "@/models/TranscriptionReadModel";
 
+import { useTranscriptionsServices } from "@/services/useTranscriptionsServices";
+
 const INITIAL_STATE = {
-  transcriptions: [
-    {
-      id: 1,
-      voice: "Voice 2",
-      text: "This is a one line sentence made to show how it fits",
-    },
-    {
-      id: 2,
-      voice: "Voice 1",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ",
-    },
-    {
-      id: 3,
-      voice: "Voice 1",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-  ],
+  transcriptions: [],
 };
 
+/**
+ * Store state.
+ */
 const state = reactive({ ...INITIAL_STATE });
 
 export function useTranscriptionsStore() {
+  /**
+   * Gets index of transcription having the given id
+   * @param {Number} id
+   */
   const getIndexOfTranscriptionById = (id) => {
     return state.transcriptions.findIndex((transcription) => {
       return transcription.id === id;
     });
   };
 
+  /**
+   * Getter for transcriptions. It parses to read model.
+   */
   const transcriptions = computed(() => {
     return state.transcriptions.map((transcription) => {
       return new TranscriptionReadModel(transcription);
     });
   });
 
+  /**
+   * Toggle selection of transcription with the given id.
+   * @param {Number} id
+   */
   const toggleTranscriptionSelectionOfId = (id) => {
     const index = getIndexOfTranscriptionById(id);
     if (index < 0) return;
@@ -51,8 +51,41 @@ export function useTranscriptionsStore() {
     });
   };
 
+  /**
+   * Removes transcription with the given id.
+   * @param {Number} id
+   */
+  const deleteTranscriptionOfId = (id) => {
+    const index = getIndexOfTranscriptionById(id);
+    if (index < 0) return;
+    state.transcriptions.splice(index, 1);
+  };
+
+  const { fetchTranscriptions, createTranscriptions } =
+    useTranscriptionsServices();
+  /**
+   * Action to get all transcriptions from api and store them.
+   */
+  const getTranscriptions = async () => {
+    const [error, transcriptions] = await fetchTranscriptions();
+    if (error) return; // ideally throws or displays notification
+    state.transcriptions = [...transcriptions];
+  };
+
+  /**
+   * Action to send newly created transcriptions from the api.
+   * @param {Array} transcriptions
+   */
+  const saveTranscriptions = async (transcriptions) => {
+    const [error] = await createTranscriptions(transcriptions);
+    if (error) () => {}; // do something if error is thrown;
+  };
+
   return {
     transcriptions,
     toggleTranscriptionSelectionOfId,
+    deleteTranscriptionOfId,
+    getTranscriptions,
+    saveTranscriptions,
   };
 }
