281614821556fb99e2e6ac149f58ba651b1d5921
Edgar Dias <dias.s.edgar@gmail.com>
2023-01-25 20:50:23 +0000

    Creating icon and button components

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="c-button qa-button" @click="$emit('click')">
+    <span v-if="text.length > 0">{{ text }}</span>
+    <Icon v-if="icon.length > 0" :name="icon" />
+  </button>
+</template>
+
+<script>
+import Icon from "@/components/Icon/Icon.vue";
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+  components: {
+    Icon,
+  },
+};
+</script>
---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="qa-icon" v-if="name.length > 0" :src="computedIcon" />
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    computedIcon() {
+      return require(`@/assets/images/${this.name}.svg`);
+    },
+  },
+};
+</script>
