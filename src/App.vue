66552cca88bad839f7076267546c02ebf0a9e8a2
Edgar Dias <dias.s.edgar@gmail.com>
2023-01-26 16:18:48 +0000

    Using Layout componet and placing List component in home view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+};
+</script>
+
 <style lang="scss">
+@import "./assets/styles/variables.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $open-sans, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <DefaultLayout>
+    <List :list="LIST" @add="ADD_ITEM" @delete="DELETE_ITEM" />
+  </DefaultLayout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import List from "@/components/List/List.vue";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "HomeView",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters("list/", ["LIST"]),
+  },
+  methods: {
+    ...mapActions("list/", ["ADD_ITEM", "DELETE_ITEM"]),
+  },
   components: {
-    HelloWorld,
+    List,
+    DefaultLayout,
   },
 };
 </script>
