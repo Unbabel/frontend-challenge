8cbd9dc5750bf15d8109c967af4a2ba99fcaaf56
Edgar Dias <dias.s.edgar@gmail.com>
2023-01-27 18:08:15 +0000

    Fixing issue with duplicate ids and enabling the editing of item

--- src/store/list.js
+++ src/store/list.js
@@ -1,4 +1,10 @@
 import Services from "@/services/index.js";
+
+/* Simple counter variable to simulate the incrementation
+of the Id number for each added item and avoid Id duplication
+No need to store it in the store  */
+let counter = 0;
+
 export default {
   namespaced: true,
   state: () => ({
@@ -6,18 +12,12 @@ export default {
   }),
   mutations: {
     ADD_ITEM(state, payload) {
-      console.log(state, payload);
       state.list.push(payload);
     },
     DELETE_ITEM(state, payload) {
-      console.log(state, payload);
       state.list = state.list.filter((item) => item.id !== payload.id);
     },
-    UPDATE_ITEM(state, payload) {
-      console.log(state, payload);
-    },
     UPDATE_LIST(state, payload) {
-      console.log(state, payload);
       state.list = payload;
     },
   },
@@ -25,23 +25,26 @@ export default {
     async GET_LIST({ commit }) {
       const list = await Services.list.GET_LIST();
       commit("UPDATE_LIST", list);
+      counter = list.length;
     },
     async POST_LIST({ dispatch }, payload) {
       await Services.list.POST_LIST(payload);
       dispatch("GET_LIST");
     },
-    ADD_ITEM({ commit, getters }) {
-      let newItem = Services.listItem.ITEM_TEMPLATE;
-      //Add logic to find for missing id and not have duplicates
-      newItem = { ...newItem, id: getters.LIST.length + 1 };
+    ADD_ITEM({ commit }) {
+      let newItem = new Services.listItem.ITEM_TEMPLATE();
+      counter++;
+      newItem = { ...newItem, id: counter };
       commit("ADD_ITEM", newItem);
     },
     DELETE_ITEM({ commit }, payload) {
-      console.log("delete", payload);
       commit("DELETE_ITEM", payload);
     },
-    UPDATE_ITEM({ commit }, payload) {
-      console.log(commit, payload);
+    UPDATE_ITEM({ commit, getters }, payload) {
+      const updatedList = getters.LIST.map((item) =>
+        item.id === payload.id ? { ...item, ...payload } : item
+      );
+      commit("UPDATE_LIST", updatedList);
     },
   },
   getters: {
