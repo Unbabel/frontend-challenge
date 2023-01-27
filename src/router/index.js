402d5b2a2b822d67b97fa3a11b28b20e3eee9bd0
Edgar Dias <dias.s.edgar@gmail.com>
2023-01-27 18:13:26 +0000

    Creation of error page and altering router to display it if the route is invalid

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,18 @@ Vue.use(VueRouter);
 
 const routes = [
   {
+    path: "*",
+    name: "error",
+    // route level code-splitting
+    // this generates a separate chunk (error.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "error" */ "../views/ErrorView.vue"),
+  },
+  {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,16 @@
+<template>
+  <DefaultLayout>
+    <h1>Page doesn't exist</h1>
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+
+export default {
+  name: "ErrorView",
+  components: {
+    DefaultLayout,
+  },
+};
+</script>
