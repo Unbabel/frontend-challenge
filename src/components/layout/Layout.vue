690357a209f4fe4ecca4b5e1fcc9571f35368ea7
186412 <enker2004@hotmail.com>
2023-02-16 21:00:07 +0000

    feat: added default layout component

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TopBar from '../top-bar/TopBar.vue';
+import Content from '../content/Content.vue';
+
+</script>
+
+<template>
+  <div class="layout-container">
+    <TopBar />
+    <Content>
+      <slot />
+    </Content>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/layout/__tests__/Layout.spec.js
@@ -0,0 +1,32 @@
+import { describe, it, expect, beforeEach } from "vitest";
+
+import { mount } from "@vue/test-utils";
+
+// Components
+import Layout from "../Layout.vue";
+import TopBar from "../../top-bar/TopBar.vue";
+import Content from "../../content/Content.vue";
+
+describe("Layout", () => {
+	let wrapper;
+
+	beforeEach(() => {
+		// Arrange
+		wrapper = mount(Layout, { propsData: {} });
+	});
+
+	it("matches snapshot", () => {
+		// Assert
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders properly", () => {
+		// Act
+		const topbar = wrapper.findComponent(TopBar);
+		const content = wrapper.findComponent(Content);
+
+		// Assert
+		expect(topbar.exists()).toBe(true);
+		expect(content.exists()).toBe(true);
+	});
+});
