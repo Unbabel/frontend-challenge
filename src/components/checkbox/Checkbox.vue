705e0624ba2b970baf9dc19bded77810ad7facf1
186412 <enker2004@hotmail.com>
2023-02-16 23:33:29 +0000

    feat: added checkbox

---
+++ src/components/checkbox/Checkbox.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <input class="custom-checkbox" type="checkbox" checked>
+</template>
+
+<style scoped>
+.custom-checkbox {
+  width: 1rem;
+  height: 1rem;
+}
+</style>
