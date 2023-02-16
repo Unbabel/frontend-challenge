bff1832a25a56f3b39cc21215f7e63a19fd23165
186412 <enker2004@hotmail.com>
2023-02-16 21:00:56 +0000

    feat: added transcriptions view

---
+++ src/views/TranscriptionsView.vue
@@ -0,0 +1,45 @@
+<script setup>
+// Components
+import Item from '../components/item/Item.vue';
+import IconButton from '../components/icon-button/IconButton.vue';
+</script>
+
+<template>
+  <div class="transcriptions-container">
+    <div class="transcriptions-inner-container">
+      <Item />
+    </div>
+    <div class="transcriptions-button-div">
+      <IconButton icon='src/assets/add-row.svg' />
+    </div>
+
+  </div>
+</template>
+
+<style>
+.transcriptions-container {
+  margin: 1.5rem auto;
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+}
+
+.transcriptions-inner-container {
+  height: 200px;
+  background-color: var(--palette-white);
+  border: 1px solid var(--palette-lighter-grey);
+}
+
+.transcriptions-button-div {
+  width: fit-content;
+  margin: 1.5rem auto 0 auto;
+}
+
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
