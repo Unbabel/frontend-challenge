e7d31bdf1662f01bda86ff7cb35390d76f77335a
186412 <enker2004@hotmail.com>
2023-02-16 20:58:41 +0000

    feat: added icon button component

---
+++ src/components/icon-button/IconButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+const emit = defineEmits(['onClickCallback']);
+
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+});
+
+const onClickHandler = () => emit('onClickCallback');
+
+</script>
+
+<template>
+  <button class="icon-button" @click="onClickHandler" data-test="action">
+    <img :src="icon" alt="action-icon" />
+  </button>
+</template>
+
+<style scoped>
+.icon-button {
+  background-color: transparent;
+  border: none;
+}
+
+.icon-button:hover {
+  cursor: pointer;
+}
+</style>
