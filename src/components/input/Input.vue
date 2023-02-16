dd146fb32bf22fa754d310cdbb53eafa82f18f31
186412 <enker2004@hotmail.com>
2023-02-16 23:33:50 +0000

    feat: added input component

---
+++ src/components/input/Input.vue
@@ -0,0 +1,38 @@
+<script setup>
+const emit = defineEmits(['onChangeCallback']);
+
+defineProps({
+  value: {
+    type: String,
+  },
+  placeholder: {
+    type: String,
+    default: 'Insert text here'
+  },
+  customStyles: {
+    type: String,
+    default: ''
+  }
+});
+
+const onChangeHandler = (ev) => emit('onChangeCallback', ev.target.value);
+
+</script>
+
+<template>
+  <input :class="customStyles" :value="value" @input="onChangeHandler" :placeholder="placeholder" />
+</template>
+
+<style scoped>
+input {
+  width: 100%;
+  border: 1px solid transparent;
+  outline: none;
+}
+
+input:focus {
+  border: 1px solid var(--palette-lighter-grey);
+  border-radius: 3px;
+  background-color: var(--palette-dirty-white);
+}
+</style>
