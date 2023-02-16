0713a63c38f84c4f8127aac9a6e4efb26665ae53
186412 <enker2004@hotmail.com>
2023-02-16 21:01:06 +0000

    feat: added 404 view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>404 - Not Found</h1>
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
