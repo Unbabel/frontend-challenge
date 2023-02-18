c98c9aa2cc4585e348d1522fbda140b262db0d2b
186412 <enker2004@hotmail.com>
2023-02-18 20:24:48 +0000

    feat: added default page component

---
+++ src/components/default-page/DefaultPage.vue
@@ -0,0 +1,74 @@
+<script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+  },
+  pageLabel: {
+    type: String,
+  },
+  btnLabel: {
+    type: String,
+  },
+  btnHandler: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="default-page-container flex-center">
+    <img v-if="imageSrc" class="default-page-img" :src="imageSrc" alt="default-page-img">
+    <span v-if="pageLabel" class="default-page-label">{{ pageLabel }}</span>
+    <button v-if="btnLabel && btnHandler" class="default-page-btn" @click="btnHandler">{{ btnLabel }}</button>
+  </div>
+</template>
+
+<style>
+.default-page-container {
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  padding-top: 4rem;
+  flex-direction: column;
+  row-gap: 2rem;
+}
+
+.default-page-img {
+  width: 45%;
+}
+
+.default-page-label {
+  width: 80%;
+  text-align: center;
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: var(--palette-dark);
+}
+
+.default-page-btn {
+  font-size: 1.3rem;
+  font-weight: 500;
+  color: var(--palette-white);
+  border: none;
+  border-radius: 10px;
+  background-color: var(--palette-purple);
+  padding: 0.5rem;
+  cursor: pointer;
+}
+
+@media (max-width: 768px) {
+
+  .default-page-label,
+  .default-page-btn {
+    font-size: 1rem;
+  }
+}
+
+@media (max-width: 400px) {
+
+  .default-page-label,
+  .default-page-btn {
+    font-size: 0.7rem;
+  }
+}
+</style>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,67 +1,24 @@
 <script>
-export default {
-  methods: {
-    onBtnClickHandler() {
-      console.log(this.$router.push('/'));
-    }
-  }
-}
+	import DefaultPage from "../components/default-page/DefaultPage.vue";
+	export default {
+		components: {
+			DefaultPage,
+		},
+		methods: {
+			onBtnClickHandler() {
+				this.$router.push("/");
+			},
+		},
+	};
 </script>
 
 <template>
-  <div class="not-found-container flex-center">
-    <img class="not-found-img" src="src/assets/not-found.svg" alt="not-found-img">
-    <span class="not-found-label">Ah, the dreaded 404. Let's get you back on track.</span>
-    <button class="not-found-btn" @click="onBtnClickHandler">Take Me Home</button>
-  </div>
+	<DefaultPage
+		image-src="src/assets/not-found.svg"
+		page-label="Ah, the dreaded 404. Let's get you back on track."
+		btn-label="Take Me Home"
+		:btn-handler="onBtnClickHandler"
+	/>
 </template>
 
-<style>
-.not-found-container {
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  padding-top: 4rem;
-  flex-direction: column;
-  row-gap: 2rem;
-}
-
-.not-found-img {
-  width: 45%;
-}
-
-.not-found-label {
-  width: 80%;
-  text-align: center;
-  font-size: 1.3rem;
-  font-weight: 600;
-  color: var(--palette-dark);
-}
-
-.not-found-btn {
-  font-size: 1.3rem;
-  font-weight: 500;
-  color: var(--palette-white);
-  border: none;
-  border-radius: 10px;
-  background-color: var(--palette-purple);
-  padding: 0.5rem;
-  cursor: pointer;
-}
-
-@media (max-width: 768px) {
-
-  .not-found-label,
-  .not-found-btn {
-    font-size: 1rem;
-  }
-}
-
-@media (max-width: 400px) {
-
-  .not-found-label,
-  .not-found-btn {
-    font-size: 0.7rem;
-  }
-}
-</style>
+<style></style>
