fd6cdf2aabe56d83691d6bde3827dc5c7ec54bf6
186412 <enker2004@hotmail.com>
2023-02-18 20:11:05 +0000

    feat: 404 page definition

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,13 +1,67 @@
+<script>
+export default {
+  methods: {
+    onBtnClickHandler() {
+      console.log(this.$router.push('/'));
+    }
+  }
+}
+</script>
+
 <template>
-  <h1>404 - Not Found</h1>
+  <div class="not-found-container flex-center">
+    <img class="not-found-img" src="src/assets/not-found.svg" alt="not-found-img">
+    <span class="not-found-label">Ah, the dreaded 404. Let's get you back on track.</span>
+    <button class="not-found-btn" @click="onBtnClickHandler">Take Me Home</button>
+  </div>
 </template>
 
 <style>
-/* @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+.not-found-container {
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  padding-top: 4rem;
+  flex-direction: column;
+  row-gap: 2rem;
+}
+
+.not-found-img {
+  width: 45%;
+}
+
+.not-found-label {
+  width: 80%;
+  text-align: center;
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: var(--palette-dark);
+}
+
+.not-found-btn {
+  font-size: 1.3rem;
+  font-weight: 500;
+  color: var(--palette-white);
+  border: none;
+  border-radius: 10px;
+  background-color: var(--palette-purple);
+  padding: 0.5rem;
+  cursor: pointer;
+}
+
+@media (max-width: 768px) {
+
+  .not-found-label,
+  .not-found-btn {
+    font-size: 1rem;
   }
-} */
+}
+
+@media (max-width: 400px) {
+
+  .not-found-label,
+  .not-found-btn {
+    font-size: 0.7rem;
+  }
+}
 </style>
