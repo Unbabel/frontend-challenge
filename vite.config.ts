437f88164c64d8b8b531d0e617d78d00db30532e
vitalino silva <vito@helu.io>
2023-03-28 15:12:17 +0100

    github pages base url settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/frontend-challenge/',
   define: {
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
